345e4863553159e21ef809e7bf174ead806c4953
Anatoly Nevzorov <tolnevz@gmail.com>
2020-03-15 19:03:21 +0300

    First commit

---
+++ src/components/News.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="container">
+    <h1 class="main-color text-center">Latest News</h1>
+    <div class="properties">
+      <form v-on:submit.prevent>
+        <div class="form-item">
+          <label for="country">Choose country</label>
+          <select name="country" v-model="selectedCountry">
+            <option v-for="country in countries" :key="country.id" :value="country.code">{{country.name}}</option>
+          </select>
+        </div>
+        <div class="form-item">
+          <label for="category">Choose category</label>
+          <select name="country" v-model="selectedCategory">
+            <option v-for="category in categories" :key="category.id" :value="category.code">{{category.name}}</option>
+          </select>
+        </div>
+        <div class="form-item">
+          <label for="category">Enter keyword</label>
+          <input type="text" name="keyword" v-model="keyword">
+        </div>
+        <div class="form-item">
+          <button class="btn btn-block" @click="getNews">Show News</button>
+        </div>
+      </form>
+    </div>
+    <div class="news" v-if="news">
+      <div class="news-item" v-for="item in news" :key="item.id">
+        <a :href="item.url" target="_blank">
+          <div class="image" :style="{ backgroundImage: 'url(' + item.urlToImage + ')' }"></div>
+          <div class="content">
+            <h4>{{ item.title }}</h4>
+            <p>{{ item.description }}</p>
+            <div class="main-color source">
+              <div class="date">
+                <span class="news-date"><i class="far fa-calendar-plus"></i> {{ item.publishedAt.substr(0, 10) }}</span>
+                <span class="news-time"><i class="far fa-clock"></i> {{ item.publishedAt.substr(11, 5)}}</span>
+              </div>
+              <div class="author">
+                <span><i class="fas fa-angle-right"></i>{{ item.source.name }}</span>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios'
+import { newsApi, countriesList, categoryList } from '../settings.js'
+
+export default {
+  name: 'News',
+  data: function(){
+    return {
+      countries: countriesList,
+      selectedCountry: 'ru',
+      categories: categoryList,
+      selectedCategory: 'general',
+      keyword: '',
+      news: null,
+    }
+  },
+  methods: {
+    getNews(){
+      axios
+        .get('http://newsapi.org/v2/top-headlines?q='+ this.keyword +'&country='+ this.selectedCountry +'&category='+ this.selectedCategory +'&apiKey=' + newsApi)
+        .then(response => (this.news = response.data.articles));
+    }
+  },
+  mounted() {
+      this.getNews()
+  }
+}
+</script>
---
+++ src/settings.js
@@ -0,0 +1,151 @@
+module.exports = {
+  newsApi: 'caa06c1f692a4fef9f384ab3c6051e57',
+  countriesList: [
+    {
+      code: 'at',
+      name: 'Austria',
+    },
+    {
+      code: 'be',
+      name: 'Belgium',
+    },
+    {
+      code: 'bg',
+      name: 'Bulgaria',
+    },
+    {
+      code: 'ca',
+      name: 'Canada',
+    },
+    {
+      code: 'ch',
+      name: 'Switzerland',
+    },
+    {
+      code: 'cn',
+      name: 'China',
+    },
+    {
+      code: 'cz',
+      name: 'Czech Republic',
+    },
+    {
+      code: 'cz',
+      name: 'Czech Republic',
+    },
+    {
+      code: 'de',
+      name: 'Germany',
+    },
+    {
+      code: 'fr',
+      name: 'France',
+    },
+    {
+      code: 'gb',
+      name: 'United Kingdom',
+    },
+    {
+      code: 'gr',
+      name: 'Greece',
+    },
+    {
+      code: 'hu',
+      name: 'Hungary',
+    },
+    {
+      code: 'it',
+      name: 'Italy',
+    },
+    {
+      code: 'jp',
+      name: 'Japan',
+    },
+    {
+      code: 'lt',
+      name: 'Lithuania',
+    },
+    {
+      code: 'lv',
+      name: 'Latvia',
+    },
+    {
+      code: 'nl',
+      name: 'Netherlands',
+    },
+    {
+      code: 'no',
+      name: 'Norway',
+    },
+    {
+      code: 'pl',
+      name: 'Poland',
+    },
+    {
+      code: 'pt',
+      name: 'Portugal',
+    },
+    {
+      code: 'ro',
+      name: 'Romania',
+    },
+    {
+      code: 'ru',
+      name: 'Russia',
+    },
+    {
+      code: 'se',
+      name: 'Sweden',
+    },
+    {
+      code: 'si',
+      name: 'Slovenia',
+    },
+    {
+      code: 'sk',
+      name: 'Slovakia',
+    },
+    {
+      code: 'tr',
+      name: 'Turkey',
+    },
+    {
+      code: 'ua',
+      name: 'Ukraine',
+    },
+    {
+      code: 'us',
+      name: 'United States',
+    },
+  ],
+  categoryList: [
+    {
+      code: 'business',
+      name: 'Business',
+    },
+    {
+      code: 'entertainment',
+      name: 'Entertainment',
+    },
+    {
+      code: 'general',
+      name: 'General',
+    },
+    {
+      code: 'health',
+      name: 'Health',
+    },
+    {
+      code: 'science',
+      name: 'Science',
+    },
+    {
+      code: 'sports',
+      name: 'Sports',
+    },
+    {
+      code: 'technology',
+      name: 'Technology',
+    },
+  ],
+}
